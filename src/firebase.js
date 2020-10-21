93e726c5177f2985a931f90aa21f11175ce6c005
Shashank201 <svarshney17@gmail.com>
2020-10-21 20:42:09 +0530

    clean up

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCf5xJwGp2wDfoCophOamuVHqzCrkPMo8Y",
+  authDomain: "clone-a07e8.firebaseapp.com",
+  databaseURL: "https://clone-a07e8.firebaseio.com",
+  projectId: "clone-a07e8",
+  storageBucket: "clone-a07e8.appspot.com",
+  messagingSenderId: "1038520884375",
+  appId: "1:1038520884375:web:f216821c7bb95bf619ddd2",
+  measurementId: "G-3E73C6VECV",
+};
